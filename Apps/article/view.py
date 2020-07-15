3f4c4c0a6165158760b4d7ccd5fd30afe8b70ce8
Victor <vic.zkj@gmail.com>
2020-07-15 20:50:43 +0800

    2⃣️完善主页样式，Post页样式，添加博客类别，点赞，点击数目展示

--- Apps/article/view.py
+++ Apps/article/view.py
@@ -7,6 +7,13 @@ from Apps.article.model import Article, Comment
 article_bp = Blueprint('article', __name__)
 
 
+@article_bp.route('/new_post', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    cu_user = User.query.get(current_user.id)
+    return render_template('article/new_post.html', user=cu_user)
+
+
 @article_bp.route('/post', methods=['GET', 'POST'])
 def post():
     pid = request.args.get('pid', type=int)
@@ -15,8 +22,9 @@ def post():
     return render_template('article/post.html', post=post_detail, comments=comments)
 
 
-@article_bp.route('/new_post', methods=['GET', 'POST'])
-@login_required
-def new_post():
-    cu_user = User.query.get(current_user.id)
-    return render_template('article/new_post.html', user=cu_user)
+@article_bp.route('/comment', methods=['GET', 'POST'])
+def comment():
+    pid = request.args.get('pid', type=int)
+    post_detail = Article.query.get(pid)
+    comments = Comment.query.get(pid)
+    return render_template('article/post.html', post=post_detail, comments=comments)
