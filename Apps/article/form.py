53fe3cf9fc4fb7d723d58bceef97afcc0fbefb10
Victor <vic.zkj@gmail.com>
2020-07-20 22:57:06 +0800

    首页文章展示从request请求拼接改为url_for获取，添加评论展示功能，修改post页部分样式

--- Apps/article/form.py
+++ Apps/article/form.py
@@ -1,6 +1,6 @@
 from flask import flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from Apps.user.model import User
@@ -16,3 +16,10 @@ class PostForm(FlaskForm):
     #     super(PostForm, self).__init__(*args, **kwargs)
     #     self.category.choices = [(category.id, category.name)
     #                              for category in Category.query.order_by(Category.name).all()]
+
+
+class CommentForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
+    body = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField()
--- Apps/article/view.py
+++ Apps/article/view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 
-from Apps.article.form import PostForm
+from Apps.article.form import PostForm, CommentForm
 from Apps.user.model import User
 
 from Apps.article.model import Comment, ArticleType, Article
@@ -28,17 +28,23 @@ def new_post():
     return render_template('article/new_post.html', form=form)
 
 
-@article_bp.route('/post', methods=['GET', 'POST'])
-def post():
-    pid = request.args.get('pid', type=int)
-    post_detail = Article.query.get(pid)
-    comments = Comment.query.get(pid)
-    return render_template('article/post.html', post=post_detail, comments=comments)
-
-
-@article_bp.route('/comment', methods=['GET', 'POST'])
-def comment():
-    pid = request.args.get('pid', type=int)
-    post_detail = Article.query.get(pid)
-    comments = Comment.query.get(pid)
-    return render_template('article/post.html', post=post_detail, comments=comments)
+@article_bp.route('/post/<int:pid>', methods=['GET', 'POST'])
+def show_post(pid):
+    post = Article.query.get_or_404(pid)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(comment=form.body.data,
+                          email=form.email.data,
+                          author=form.author.data,
+                          article_id=pid)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been published.')
+        return redirect(url_for('article.show_post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    # if page == -1:
+    #     page = (post.comments.count() - 1)
+    pagination = Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.create_time.asc()).paginate(
+        page=page, per_page=2)
+    comments = pagination.items
+    return render_template('article/post.html', post=post, form=form, comments=comments, pagination=pagination)
